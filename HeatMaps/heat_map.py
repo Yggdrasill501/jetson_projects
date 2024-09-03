ca4901f312d741b93f6c3994bcbcff64922b4d3c
Filip Žitný <filipzitny@gmail.com>
2024-09-03 12:47:18 +0200

    Restructured: moved original models into OrignialModels

--- HeatMaps/heat_map.py
+++ HeatMaps/heat_map.py
@@ -1,86 +1,84 @@
- #!/usr/bin/env python3
-#
-# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-#
-
 import sys
 import argparse
-from jetson_utils import videoSource, videoOutput, Log
+import csv
+import numpy as np
+from jetson_inference import depthNet
+from jetson_utils import videoSource, videoOutput, cudaOverlay, cudaDeviceSynchronize, Log
 
+from depthnet_utils import depthBuffers
 
-# parse command line
-parser = argparse.ArgumentParser(description="Test various video streaming APIs", 
-                                 formatter_class=argparse.RawTextHelpFormatter, 
-                                 epilog=videoSource.Usage() + videoOutput.Usage() + Log.Usage())
+# parse the command line
+parser = argparse.ArgumentParser(description="Mono depth estimation on a video/image stream using depthNet DNN.",
+                                 formatter_class=argparse.RawTextHelpFormatter,
+                                 epilog=depthNet.Usage() + videoSource.Usage() + videoOutput.Usage() + Log.Usage())
 
-parser.add_argument("input", type=str, default="/dev/video0", nargs='?', help="URI of the input stream")
-parser.add_argument("output", type=str, default="images/test/test_video.mp4", nargs='?', help="URI of the output stream")
+parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="fcn-mobilenet", help="pre-trained model to load, see below for options")
+parser.add_argument("--visualize", type=str, default="input,depth", help="visualization options (can be 'input' 'depth' 'input,depth'")
+parser.add_argument("--depth-size", type=float, default=1.0, help="scales the size of the depth map visualization, as a percentage of the input size (default is 1.0)")
+parser.add_argument("--filter-mode", type=str, default="linear", choices=["point", "linear"], help="filtering mode used during visualization, options are:\n  'point' or 'linear' (default: 'linear')")
+parser.add_argument("--colormap", type=str, default="viridis-inverted", help="colormap to use for visualization (default is 'viridis-inverted')",
+                                  choices=["inferno", "inferno-inverted", "magma", "magma-inverted", "parula", "parula-inverted",
+                                           "plasma", "plasma-inverted", "turbo", "turbo-inverted", "viridis", "viridis-inverted"])
+
+parser.add_argument("--csv-output", type=str, default="depth_map.csv", help="filename to save depth map data to CSV")
 
 try:
-	args = parser.parse_known_args()[0]
+    args = parser.parse_known_args()[0]
 except:
-	print("")
-	parser.print_help()
-	sys.exit(0)
-    
-    
-# create video source
-input = videoSource(args.input, 
-                    argv=sys.argv,
-                    options={
-                        'width': 1920,
-                        'height': 1080,
-                        'framerate': 30, 
-                    })
-           
-# output
-output = videoOutput(args.output,
-                     argv=sys.argv,
-                     options={
-                        'bitrate': 2500000,
-                        'codec': 'h264'
-                     })
-                     
-# capture frames until EOS or user exits
-numFrames = 0
+    print("")
+    parser.print_help()
+    sys.exit(0)
 
+# load the segmentation network
+net = depthNet(args.network, sys.argv)
+
+# create buffer manager
+buffers = depthBuffers(args)
+
+# create video sources & outputs
+input = videoSource(args.input, argv=sys.argv)
+output = videoOutput(args.output, argv=sys.argv)
+
+# process frames until EOS or the user exits
 while True:
     # capture the next image
-    img = input.Capture()
+    img_input = input.Capture()
 
-    if img is None: # timeout
-        continue  
-        
-    if numFrames % 25 == 0 or numFrames < 15:
-        Log.Verbose(f"test-video:  captured {numFrames} frames ({img.width} x {img.height})")
-	
-    numFrames += 1
-	
-    # render the image
-    output.Render(img)
-    
+    if img_input is None:  # timeout
+        continue
+
+    # allocate buffers for this size image
+    buffers.Alloc(img_input.shape, img_input.format)
+
+    # process the mono depth and visualize
+    net.Process(img_input, buffers.depth, args.colormap, args.filter_mode)
+
+    # composite the images
+    if buffers.use_input:
+        cudaOverlay(img_input, buffers.composite, 0, 0)
+
+    if buffers.use_depth:
+        cudaOverlay(buffers.depth, buffers.composite, img_input.width if buffers.use_input else 0, 0)
+
+    # render the output image
+    output.Render(buffers.composite)
+
     # update the title bar
-    output.SetStatus("Video Test | {:d}x{:d} | {:.1f} FPS".format(img.width, img.height, output.GetFrameRate()))
-	
+    output.SetStatus("{:s} | {:s} | Network {:.0f} FPS".format(args.network, net.GetNetworkName(), net.GetNetworkFPS()))
+
+    # print out performance info
+    cudaDeviceSynchronize()
+    net.PrintProfilerTimes()
+
+    # Save depth data to CSV
+    depth_data = np.array(buffers.depth)
+    with open(args.csv_output, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in depth_data:
+            writer.writerow(row)
+
     # exit on input/output EOS
     if not input.IsStreaming() or not output.IsStreaming():
         break
-
